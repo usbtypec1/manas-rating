a53e92d93b2b7d2b720efebb0c5fe044806760a0
Eldos <eldos.baktybekov@gmail.com>
2024-07-12 15:08:02 +0600

    :bug: Fix confirm dialog

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <AppHeader/>
+    <ConfirmDialog class="mx-4"/>
     <main class="container mx-auto pt-4">
       <slot/>
     </main>
---
+++ plugins/primevue-confirmation-service.ts
@@ -0,0 +1,6 @@
+import ConfirmationService from 'primevue/confirmationservice'
+
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(ConfirmationService)
+})
