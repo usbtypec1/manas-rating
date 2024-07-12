31c2ce033d35b826befe9fb83430be0c24a3acb7
Eldos <eldos.baktybekov@gmail.com>
2024-07-12 15:56:55 +0600

    :sparkles: Add photos blacklist

--- services/photos.ts
+++ services/photos.ts
@@ -1,4 +1,13 @@
-export const PHOTOS_BLACKLIST_APPLICATION_NUMBERS: string[] = []
+export const PHOTOS_BLACKLIST_APPLICATION_NUMBERS: string[] = [
+  '24005554',
+  '24006514',
+  '24001338',
+  '24000265',
+  '24007410',
+  '24000378',
+  '24005199',
+  '24004028',
+]
 
 export const buildApplicantPhotoUrl = (applicantId: string): string => {
   return `https://abiturient.manas.edu.kg/page/uploads/photo/${applicantId}.jpg`
