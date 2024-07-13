cf2a9a70d21c46bfdac87ad1405f365026c93d34
Eldos <eldos.baktybekov@gmail.com>
2024-07-13 22:24:46 +0600

    :sparkles: Update contact info

--- components/ContactInfo.vue
+++ components/ContactInfo.vue
@@ -2,7 +2,7 @@
   <h1 class="text-3xl font-bold my-3">
     Улучшенный (неофициальный) рейтинг абитуриентов университета Манас
   </h1>
-  <p class="my-2 text-xl">Наш чат в Telegram - <a class="text-violet-500" href="https://t.me/studmanas" target="_blank">@studmanas</a></p>
+  <p class="my-2 text-xl">Наш чат в Telegram - <a class="text-violet-500" href="https://t.me/studmanass" target="_blank">@studmanass</a></p>
   <p class="my-2 text-xl">Разработчик - <a class="text-violet-500" href="https://t.me/usbtypec" target="_blank">@usbtypec</a></p>
 </template>
 
